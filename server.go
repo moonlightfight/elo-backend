0f40dd69d42d744627f1fe1aa18a9d250a57125c
Corey Lanier <iam@coreylanier.com>
2021-06-05 18:54:32 -0400

    fixed the way the database is connected to

--- server.go
+++ server.go
@@ -1,21 +1,15 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/gorilla/mux"
 	c "github.com/moonlightfight/elo-backend/config"
 	"github.com/moonlightfight/elo-backend/routes/admin"
 	"github.com/moonlightfight/elo-backend/routes/tournament"
 	"github.com/spf13/viper"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
-
-var client *mongo.Client
 
 func main() {
 	// Set the file name of the configurations file
@@ -38,14 +32,11 @@ func main() {
 	if err != nil {
 		fmt.Printf("Unable to decode into struct, %v", err)
 	}
-	fmt.Println("Start writing code!")
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	clientOptions := options.Client().ApplyURI(fmt.Sprintf("mongodb+srv://%s:%s@cluster0.ucnph.mongodb.net/%s?retryWrites=true&w=majority", configuration.Database.DBUser, configuration.Database.DBPass, configuration.Database.DBName))
 	port := fmt.Sprintf(":%d", configuration.Server.Port)
-	mongo.Connect(ctx, clientOptions)
 	router := mux.NewRouter()
 	router.HandleFunc("/api/admin", admin.CreateAdminEndpoint).Methods("POST")
 	router.HandleFunc("/api/admin/login", admin.AdminLoginEndpoint).Methods("POST")
 	router.HandleFunc("/api/tournament/getfromweb", tournament.GetTournamentData).Methods("GET")
+	fmt.Printf("server listening on http://localhost%v", port)
 	http.ListenAndServe(port, router)
 }
