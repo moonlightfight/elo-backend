a01d66d71154f1713e7bc82f01ade2722c2e9334
Corey Lanier <iam@coreylanier.com>
2021-06-08 23:11:39 -0400

    created endpoint to retrieve all characters

--- routes/character/character.go
+++ routes/character/character.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/moonlightfight/elo-backend/database"
 	"github.com/moonlightfight/elo-backend/models"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func CreateCharacterEndpoint(response http.ResponseWriter, request *http.Request) {
@@ -29,4 +30,33 @@ func CreateCharacterEndpoint(response http.ResponseWriter, request *http.Request
 		fmt.Println(err)
 	}
 	json.NewEncoder(response).Encode(result)
+}
+
+func GetCharactersEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	var characters []models.Character
+	client, err := database.ConfigDB()
+	if err != nil {
+		fmt.Println(err)
+	}
+	collection := client.Database("test").Collection("Character")
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	defer cursor.Close(ctx)
+	for cursor.Next(ctx) {
+		var character models.Character
+		cursor.Decode(&character)
+		characters = append(characters, character)
+	}
+	if err := cursor.Err(); err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(characters)
 }
