5079972aca2abf53ae738567abb406d1a637eae6
Corey Lanier <iam@coreylanier.com>
2021-06-21 23:25:53 -0400

    made sure there's no zero points given

--- routes/tournament/helpers/helpers.go
+++ routes/tournament/helpers/helpers.go
@@ -4,5 +4,8 @@ import "math"
 
 func CalculateTournamentPoints(numPlayers, placing int) int {
 	score := math.Abs(float64(placing - numPlayers))
+	if score == 0 {
+		score++
+	}
 	return int(score)
 }
