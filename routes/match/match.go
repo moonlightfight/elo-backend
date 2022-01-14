ede6864e700eebaec86aa9f0063e19deaad4e660
Corey Lanier <iam@coreylanier.com>
2022-01-13 21:40:12 -0500

    create functions for getting matches

--- routes/match/match.go
+++ routes/match/match.go
@@ -1,1 +1,9 @@
 package match
+
+func GetAllMatches() {}
+
+func GetMatchesByPlayer(id string) {}
+
+func GetMatchesByTournament(id string) {}
+
+func GetMatchesByPlayerAndOpponent(id string, opponentId string) {}
