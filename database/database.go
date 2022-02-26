f8270999491dd208c8db00d6b6334fcc9e2fca5a
Corey Lanier <iam@coreylanier.com>
2022-02-26 00:11:06 -0600

    create mongo instantiation package

---
+++ database/database.go
@@ -0,0 +1,43 @@
+package database
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+type DB struct {
+	client *mongo.Client
+}
+
+func Connect(dbUrl string) *DB {
+	client, err := mongo.NewClient(options.Client().ApplyURI(dbUrl))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel = context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &DB{
+		client: client,
+	}
+}
