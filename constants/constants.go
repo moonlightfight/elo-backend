61870c3db48850b68b37a047538b9758625ca722
Corey Lanier <iam@coreylanier.com>
2022-03-15 19:39:00 -0500

    remove specific call for mongodb uri for generic

--- constants/constants.go
+++ constants/constants.go
@@ -7,17 +7,6 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func GetDbUri() string {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatalf("Some error occured. Err: %s", err)
-	}
-
-	mongodbUri := os.Getenv("MONGODB_URI")
-
-	return mongodbUri
-}
-
 func GetEnvVar(varName string) string {
 	err := godotenv.Load(".env")
 	if err != nil {
--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -108,5 +108,5 @@ type queryResolver struct{ *Resolver }
 //  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
 //    it when you're done.
 //  - You have helper methods in this file. Move them out to keep these resolver files clean.
-var mongodbUri = constants.GetDbUri()
+var mongodbUri = constants.GetEnvVar("MONGODB_URI")
 var db = database.Connect(mongodbUri)
